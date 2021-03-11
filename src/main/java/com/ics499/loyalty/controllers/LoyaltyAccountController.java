a67b2a18935fa639ef3d408595058267b4197dbd
Alayna Buysse <alaynagrace@comcast.net>
2021-03-11 16:46:43 -0600

    move code

--- src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
+++ src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
@@ -1,5 +1,19 @@
 package com.ics499.loyalty.controllers;
 
 public class LoyaltyAccountController {
-
+	private void RedeemRewards(Reward rewards) {
+		
+	}
+	
+	private void displayAllMembers(MemberList memberList) {
+		
+	}
+	
+	private void addMember(Customer customer) {
+		
+	}
+	
+	private LoyaltyAccount findMember(LoyaltyAccount loyaltyacc) {
+		
+	}
 }
