0573a2b3ae86f2e5dfe5711a819f374268b1237d
Alayna Buysse <alaynagrace@comcast.net>
2021-03-11 20:59:45 -0600

    add routes

--- src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
+++ src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
@@ -1,29 +1,59 @@
 package com.ics499.loyalty.controllers;
 
+import com.ics499.loyalty.model.LoyaltyAccount;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;  
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.PathVariable;  
+import org.springframework.web.bind.annotation.PostMapping;  
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;  
+
+// only needed for basic backend testing
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/loyalty_account")
 public class LoyaltyAccountController {
+    // for demo purposes only
+    private HashMap<String, LoyaltyAccount> accounts = new HashMap<String, LoyaltyAccount>();
+    private int accountIncrement = 1;
 
-    // @RequestMapping(value ="/redeem", method = RequestMethod.PUT)
-	// public void RedeemRewards(Reward rewards) {
-		
-	// }
-    
-    @RequestMapping("/display")
-	public String displayAllMembers() {
-		return "{\"users\": [{\"name\": \"Bob\", \"account_tier\": \"Gold\"}, {\"name\": \"Jenny\", \"account_tier\": \"Gold\"}]}";
+    @GetMapping(path = "/create_demo")
+    public String createAccounts() {
+        accounts.put("bob@gmail.com", new LoyaltyAccount(1, "bob@gmail.com", 100, 500, 600, "Gold"));
+        accounts.put("jenny@gmail.com", new LoyaltyAccount(2, "jenny@gmail.com", 100, 300, 1000, "Platinum"));
+        accountIncrement = 3;
+        return "{\"message\": \"Bob and Jenny created.\"}";
+    }
+
+    @PutMapping(path = "/redeem")
+	public String RedeemRewards() {
+        
+		return "{\"message\": \"Rewards redeemed\"}";
 	}
     
-    // @RequestMapping(value = "/add", method = RequestMethod.POST)
-	// public void addMember(Customer customer) {
-		
-	// }
+    @GetMapping(path = "/display")
+	public String displayAllMembers() {
+        String users = "{\"users\": [";
+        for(String i : accounts.keySet()) {
+            users = users + accounts.get(i).getJSON() + ",";
+        }
+        users = users + "]}";
+		return users;
+	}
     
-    // @RequestMapping(value = "/find", method = RequestMethod.GET)
-	// public LoyaltyAccount findMember(LoyaltyAccount loyaltyacc) {
-		
-	// }
+    @PostMapping(path = "/add")
+	public String addMember(@RequestBody String email) {
+        accounts.put(email, new LoyaltyAccount(accountIncrement, email, 0, 0, 0, "Silver"));
+        accountIncrement++;
+        return "{\"message\": \"New member " + email + " added.\"}";
+	}
+    
+    @GetMapping(path = "/find/{email}")
+	public String findMember(@PathVariable("email") String email) {
+		return accounts.get(email).getJSON();
+	}
 }
--- src/main/java/com/ics499/loyalty/model/LoyaltyAccount.java
+++ src/main/java/com/ics499/loyalty/model/LoyaltyAccount.java
@@ -1,60 +1,68 @@
-// package com.ics499.loyalty.model;
+package com.ics499.loyalty.model;
 
-// public class LoyaltyAccount {
+public class LoyaltyAccount {
 
-// 	int accountID;
-// 	String email;
-// 	int pointsBalance;
-// 	int ytdPoints;
-// 	int lifetimePoints;
-// 	TierStrategy tierStrat;
+	int accountID;
+	String email;
+	int pointsBalance;
+	int ytdPoints;
+	int lifetimePoints;
+	String tierStrat;
 	
-// 	public LoyaltyAccount(int aID, String e, int pB, int ytdP, int lP, TierStrategy tS) {
-// 		accountID = aID;
-// 		email = e;
-// 		pointsBalance = pB;
-// 		ytdPoints = ytdP;
-// 		lifetimePoints = lP;
-// 		tierStrat = tS;		
-// 	}
+	public LoyaltyAccount(int aID, String e, int pB, int ytdP, int lP, String tS) {
+		accountID = aID;
+		email = e;
+		pointsBalance = pB;
+		ytdPoints = ytdP;
+		lifetimePoints = lP;
+		tierStrat = tS;		
+	}
 	
-// 	//Getters and Setters
+	//Getters and Setters
 	
-// 	public int getAccountID() {
-// 		return accountID;
-// 	}
-// 	public void setAccountID(int accountID) {
-// 		this.accountID = accountID;
-// 	}
-// 	public String getEmail() {
-// 		return email;
-// 	}
-// 	public void setEmail(String email) {
-// 		this.email = email;
-// 	}
-// 	public int getPointsBalance() {
-// 		return pointsBalance;
-// 	}
-// 	public void setPointsBalance(int pointsBalance) {
-// 		this.pointsBalance = pointsBalance;
-// 	}
-// 	public int getYtdPoints() {
-// 		return ytdPoints;
-// 	}
-// 	public void setYtdPoints(int ytdPoints) {
-// 		this.ytdPoints = ytdPoints;
-// 	}
-// 	public int getLifetimePoints() {
-// 		return lifetimePoints;
-// 	}
-// 	public void setLifetimePoints(int lifetimePoints) {
-// 		this.lifetimePoints = lifetimePoints;
-// 	}
-// 	public TierStrategy getTierStrat() {
-// 		return tierStrat;
-// 	}
-// 	public void setTierStrat(TierStrategy tierStrat) {
-// 		this.tierStrat = tierStrat;
-// 	}
-// }
+	public int getAccountID() {
+		return accountID;
+	}
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPointsBalance() {
+		return pointsBalance;
+	}
+	public void setPointsBalance(int pointsBalance) {
+		this.pointsBalance = pointsBalance;
+	}
+	public int getYtdPoints() {
+		return ytdPoints;
+	}
+	public void setYtdPoints(int ytdPoints) {
+		this.ytdPoints = ytdPoints;
+	}
+	public int getLifetimePoints() {
+		return lifetimePoints;
+	}
+	public void setLifetimePoints(int lifetimePoints) {
+		this.lifetimePoints = lifetimePoints;
+	}
+	public String getTierStrat() {
+		return tierStrat;
+	}
+	public void setTierStrat(String tierStrat) {
+		this.tierStrat = tierStrat;
+    }
+    
+    public String getJSON() {
+        return "{\"account_id\": " + String.valueOf(this.accountID) + ", \"email\": \"" + this.email + "\", " + 
+        "\"points_balance\": " + String.valueOf(this.pointsBalance) + ", " + "\"ytd_points\": " 
+        + String.valueOf(this.ytdPoints) + ", " +  "\"lifetime_points\": " 
+        + String.valueOf(this.lifetimePoints) + ", " +  "\"tier\": \"" 
+        + this.tierStrat + "\"}";
+    }
+}
 
