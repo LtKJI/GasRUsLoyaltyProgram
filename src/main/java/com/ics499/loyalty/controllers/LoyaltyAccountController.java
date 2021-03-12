d36294e92d392d030f5b6209b651547926a155b6
Alayna Buysse <alaynagrace@comcast.net>
2021-03-11 19:19:24 -0600

    fake display

--- src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
+++ src/main/java/com/ics499/loyalty/controllers/LoyaltyAccountController.java
@@ -1,19 +1,29 @@
 package com.ics499.loyalty.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/loyalty_account")
 public class LoyaltyAccountController {
-	private void RedeemRewards(Reward rewards) {
+
+    // @RequestMapping(value ="/redeem", method = RequestMethod.PUT)
+	// public void RedeemRewards(Reward rewards) {
 		
+	// }
+    
+    @RequestMapping("/display")
+	public String displayAllMembers() {
+		return "{\"users\": [{\"name\": \"Bob\", \"account_tier\": \"Gold\"}, {\"name\": \"Jenny\", \"account_tier\": \"Gold\"}]}";
 	}
-	
-	private void displayAllMembers(MemberList memberList) {
+    
+    // @RequestMapping(value = "/add", method = RequestMethod.POST)
+	// public void addMember(Customer customer) {
 		
-	}
-	
-	private void addMember(Customer customer) {
+	// }
+    
+    // @RequestMapping(value = "/find", method = RequestMethod.GET)
+	// public LoyaltyAccount findMember(LoyaltyAccount loyaltyacc) {
 		
-	}
-	
-	private LoyaltyAccount findMember(LoyaltyAccount loyaltyacc) {
-		
-	}
+	// }
 }
--- src/main/java/com/ics499/loyalty/model/LoyaltyAccount.java
+++ src/main/java/com/ics499/loyalty/model/LoyaltyAccount.java
@@ -1,60 +1,60 @@
-package com.ics499.loyalty.model;
+// package com.ics499.loyalty.model;
 
-public class LoyaltyAccount {
+// public class LoyaltyAccount {
 
-	int accountID;
-	String email;
-	int pointsBalance;
-	int ytdPoints;
-	int lifetimePoints;
-	TierStrategy tierStrat;
+// 	int accountID;
+// 	String email;
+// 	int pointsBalance;
+// 	int ytdPoints;
+// 	int lifetimePoints;
+// 	TierStrategy tierStrat;
 	
-	public LoyaltyAccount(int aID, String e, int pB, int ytdP, int lP, TierStrategy tS) {
-		accountID = aID;
-		email = e;
-		pointsBalance = pB;
-		ytdPoints = ytdP;
-		lifetimePoints = lP;
-		tierStrat = tS;		
-	}
+// 	public LoyaltyAccount(int aID, String e, int pB, int ytdP, int lP, TierStrategy tS) {
+// 		accountID = aID;
+// 		email = e;
+// 		pointsBalance = pB;
+// 		ytdPoints = ytdP;
+// 		lifetimePoints = lP;
+// 		tierStrat = tS;		
+// 	}
 	
-	//Getters and Setters
+// 	//Getters and Setters
 	
-	public int getAccountID() {
-		return accountID;
-	}
-	public void setAccountID(int accountID) {
-		this.accountID = accountID;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getPointsBalance() {
-		return pointsBalance;
-	}
-	public void setPointsBalance(int pointsBalance) {
-		this.pointsBalance = pointsBalance;
-	}
-	public int getYtdPoints() {
-		return ytdPoints;
-	}
-	public void setYtdPoints(int ytdPoints) {
-		this.ytdPoints = ytdPoints;
-	}
-	public int getLifetimePoints() {
-		return lifetimePoints;
-	}
-	public void setLifetimePoints(int lifetimePoints) {
-		this.lifetimePoints = lifetimePoints;
-	}
-	public TierStrategy getTierStrat() {
-		return tierStrat;
-	}
-	public void setTierStrat(TierStrategy tierStrat) {
-		this.tierStrat = tierStrat;
-	}
-}
+// 	public int getAccountID() {
+// 		return accountID;
+// 	}
+// 	public void setAccountID(int accountID) {
+// 		this.accountID = accountID;
+// 	}
+// 	public String getEmail() {
+// 		return email;
+// 	}
+// 	public void setEmail(String email) {
+// 		this.email = email;
+// 	}
+// 	public int getPointsBalance() {
+// 		return pointsBalance;
+// 	}
+// 	public void setPointsBalance(int pointsBalance) {
+// 		this.pointsBalance = pointsBalance;
+// 	}
+// 	public int getYtdPoints() {
+// 		return ytdPoints;
+// 	}
+// 	public void setYtdPoints(int ytdPoints) {
+// 		this.ytdPoints = ytdPoints;
+// 	}
+// 	public int getLifetimePoints() {
+// 		return lifetimePoints;
+// 	}
+// 	public void setLifetimePoints(int lifetimePoints) {
+// 		this.lifetimePoints = lifetimePoints;
+// 	}
+// 	public TierStrategy getTierStrat() {
+// 		return tierStrat;
+// 	}
+// 	public void setTierStrat(TierStrategy tierStrat) {
+// 		this.tierStrat = tierStrat;
+// 	}
+// }
 
