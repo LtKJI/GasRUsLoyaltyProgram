2b08a97b1c65a22c55f88ef2389483461e3af416
KeithIrwin <keithirw@gmail.com>
2021-03-18 17:38:53 -0500

    Basic Transaction Controller Functions

--- src/main/java/com/ics499/loyalty/controllers/TransactionController.java
+++ src/main/java/com/ics499/loyalty/controllers/TransactionController.java
@@ -1,5 +1,36 @@
 package com.ics499.loyalty.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ics499.loyalty.model.Transaction;
+import com.ics499.loyalty.repositories.TransactionRepo;
+
+@RestController
+@RequestMapping ("/transactions")
 public class TransactionController {
+
+	private TransactionRepo transactionRepo;
+	
+	@GetMapping ("/all")
+	public List<Transaction> list(){
+		return transactionRepo.findAll();
+	}
+	
+	@GetMapping ("/id")
+	public Transaction getByID (@PathVariable int id) {
+		return transactionRepo.getOne(id);
+	}
+	
+	@PostMapping
+	public Transaction add (@RequestBody final Transaction transaction) {
+		return transactionRepo.saveAndFlush(transaction);
+	}
 
 }
